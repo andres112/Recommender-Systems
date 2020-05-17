5083e7e3f1d029808db13e64b59a8a1760126afe
Andres Dorado <andres.dorado90@gmail.com>
2020-05-17 22:50:06 +0200

    GROUP MODELING: AGGREGATION STRATEGIES
    Average,  Most Pleasure and Average without misery strategies

--- 5_ex/main.py
+++ 5_ex/main.py
@@ -21,21 +21,72 @@ Groups = {
     15: ['U39', 'U45', 'U47']
 }
 
-def get_prediction():
+# Compute the prediction based on the 
+def get_prediction(top):
     prediction = pd.DataFrame(columns=['movie','rating'])
     for movie in top.index.values:
         if(similarity.loc[0,movie+'.1'] > 0):
+            # Select the films with the highest rating
             duplicateDFRow = similarity[[movie,movie+'.1']][similarity[movie+'.1'] == similarity.loc[0,movie+'.1']]
             duplicateDFRow['movie'] = duplicateDFRow[movie]
-            duplicateDFRow['rating'] = duplicateDFRow[movie+'.1'] * top.loc[movie]
+            duplicateDFRow['rating'] = duplicateDFRow[movie+'.1'] * top.loc[movie] # Predicted Ratings Calculation
             
-            prediction = pd.concat([prediction, duplicateDFRow[['movie', 'rating']]])
+            prediction = pd.concat([prediction, duplicateDFRow[['movie', 'rating']]], ignore_index=True)
     return prediction
 
-# AgrS: LEAST MISERY
+# TASK related with Table 1
+# AgrS: LEAST MISERY for thw whole Table 1
+print("*****************************\nTask Number 1\n")
 Group_recomendation={}
 for group in Groups:
     top = ratings[Groups[group]].min(axis=1).sort_values(ascending=False)[:10]
-    prediction = get_prediction()
+    prediction = get_prediction(top)
+    Group_recomendation[group] = prediction.sort_values('rating', ascending=False).drop_duplicates(subset ="movie")[:5]
+    # print(f'\nGroup {group}:\n',Group_recomendation[group])
+
+# TASK related with Table 2
+
+print("\n*****************************\nTask Number 2\n")
+# Only use the U5 and U15
+Groups = {
+    5: ['U31', 'U32', 'U33', 'U34', 'U40', 'U42', 'U18', 'U21', 'U48'],
+    15: ['U39', 'U45', 'U47']
+}
+# AgrS: AVERAGE STRATEGY
+Group_recomendation={}
+print("\n* Average STRATEGY\n")
+for group in Groups:
+    # Get mean rating between users of the group
+    top = ratings[Groups[group]].mean(axis=1).sort_values(ascending=False)[:10]
+    prediction = get_prediction(top)
     Group_recomendation[group] = prediction.sort_values('rating', ascending=False).drop_duplicates(subset ="movie")[:5]
     print(f'\nGroup {group}:\n',Group_recomendation[group])
+
+# AgrS: Most Pleasure STRATEGY
+Group_recomendation={}
+print("\n* Most Pleasure STRATEGY\n")
+for group in Groups:
+    # Get the max rating between users of the group
+    top = ratings[Groups[group]].max(axis=1).sort_values(ascending=False)[:10]
+    prediction = get_prediction(top)
+    Group_recomendation[group] = prediction.sort_values('rating', ascending=False).drop_duplicates(subset ="movie")[:5]
+    print(f'\nGroup {group}:\n',Group_recomendation[group])
+
+# AgrS: Average without misery STRATEGY
+Group_recomendation={}
+print("\n* Average without misery STRATEGY\n")
+# Define the threshold to define the misery value
+threshold = 2
+for group in Groups:
+    # First get the users without misery value
+    wo_misery = (ratings[Groups[group]] < threshold).sum()
+    index = wo_misery[wo_misery == 0].index.values
+    # Just computing the group predictions if there are users
+    if(len(index) > 0):
+        # Get mean rating between users of the group
+        top = ratings[index].mean(axis=1).sort_values(ascending=False)[:10]
+        prediction = get_prediction(top)
+        Group_recomendation[group] = prediction.sort_values('rating', ascending=False).drop_duplicates(subset ="movie")[:5]
+        print(f'\nGroup {group}:\n',Group_recomendation[group])
+    else:
+        print(f'\nGroup {group}:\nAll user of the group has at leas one movie rated under the threshold.')
